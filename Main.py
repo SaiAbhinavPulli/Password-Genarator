bd27a6604902163624f930484734fb63533f2c86
SaiAbhinavPulli <82861100+SaiAbhinavPulli@users.noreply.github.com>
2021-04-20 10:59:37 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,5 @@
+import random # pip install random
+passlen = int(input("enter the length of password"))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
